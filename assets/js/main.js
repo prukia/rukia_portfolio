7cedece45f417f5223e5c494034ad740fdc8d139
prukia <rukia.sm.23@gmail.com>
2020-06-22 07:09:27 -0500

    updated photos on site

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,7 @@
 
         if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
         }
         return false;
       }
